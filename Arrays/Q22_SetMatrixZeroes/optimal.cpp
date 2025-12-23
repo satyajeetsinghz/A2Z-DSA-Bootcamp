28ece27469b2ca7c6b9284af7e40bcd8b2efaf61
SATYA <work.satyajeetsingh@gmail.com>
2025-12-24 00:05:22 +0530

    "revisit Set Matrix Zeroes using optimal approach"

--- Arrays/Q22_SetMatrixZeroes/optimal.cpp
+++ Arrays/Q22_SetMatrixZeroes/optimal.cpp
@@ -91,5 +91,13 @@ int main()
         }
     }
 
+    // Output the final zeroed matrix
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
